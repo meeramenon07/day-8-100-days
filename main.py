09830674fc8d27f05cd3eb83c669284c7b0ab4ff
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-11-28 14:05:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+print("Wholesome Positivity Machine")
+name = input("What's your name? ")
+if name == "Meera" or name == "meera" and name.upper() or name.lower():
+  print("Hello",name.upper())
+FavThings = input("What do you enjoy doing most? ")
+
+currentDay = input("Enter the current day ")
+if currentDay == "Monday" or currentDay == "monday" and currentDay.upper() or currentDay.lower():
+  print("What's your goal for", currentDay.upper())
+  #goal = input("Enter your goal ")
+  feelingToday = input("On a scale of 1 - 10 how do you feel today? (1: awesome, 10: sad) ? ")
+  if feelingToday == "1":
+   print("That's Great! I know, you are the Best!")
+  elif feelingToday == "10":
+    print("Oh, don't worry! I know, you can do it really well!")
+  else:
+    print("Keep up the good work until you reach your goal!")
+
+elif currentDay == "Tuesday" or currentDay == "tuesday" and currentDay.upper() or CurrentDay.lower():
+  print("What's your goal for", currentDay.upper())
+  #goalDay2 = input("What's your goal for Day 2? ")
+  feelingToday = input("On a scale of 1 - 10 how do you feel today? (1: awesome, 10: sad) ? " )
+  if feelingToday == "1":
+    print("That's Great! I know, you are the Best!")
+  elif feelingToday == "10":
+    print("Oh, don't worry! I know, you can do it really well!")
+  else:
+    print("I heard, you are doing just fine, keep up the good work until you reach your goal!")
+  
+elif currentDay == "Wednesday" or currentDay == "wednesday" and currentDay.upper() or currentDay.lower():
+  print("What's your goal for", currentDay.upper())
+  #goalDay3 = input("What's your goal for Day 3? ")
+  feelingToday = input("On a scale of 1 - 10 how do you feel today? (1: awesome, 10: sad) ? " )
+  if feelingToday == "1":
+    print("That's Great! I know, you are the Best!")
+  elif feelingToday == "10":
+    print("Oh, don't worry! I know, you can do it really well!")
+  else:
+    print("I heard, you are doing just fine, keep up the good work until you reach your goal!")
+  
+elif currentDay == "Thursday" or currentDay == "thursday" and currentDay.upper() or currentDay. lower():
+  print("What's your goal for", currentDay.upper())
+  #goalDay4 = input("What's your goal for Day 4? ")
+  feelingToday = input("On a scale of 1 - 10 how do you feel today? (1: awesome, 10: sad) ? " )
+  if feelingToday == "1":
+    print("That's Great! I know, you are the Best!")
+  elif feelingToday == "10":
+    print("Oh, don't worry! I know, you can do it really well!")
+  else:
+    print("I heard, you are doing just fine, keep up the good work until you reach your goal!")
+
+elif currentDay == "Friday" or currentDay == "friday" and currentDay.upper() or currentDay.lower():
+  print("What's your goal for", currentDay.upper())
+  #goalDay5 = input("What's your goal for Day 5? ")
+  feelingToday = input("On a scale of 1 - 10 how do you feel today? (1: awesome, 10: sad) ? " )
+  if feelingToday == "1":
+    print("That's Great! I know, you are the Best!")
+  elif feelingToday == "10":
+    print("Oh, don't worry! I know, you can do it really well!")
+  else:
+    print("I heard, you are doing just fine, keep up the good work until you reach your goal!")
+ 
+elif currentDay == "Saturday" or currentDay == "saturday" and currentDay.upper() or currentDay.lower():
+  print("What's your goal for", currentDay.upper())
+  #goalDay6 = input("What's your goal for Day 6? ")
+  feelingToday = input("On a scale of 1 - 10 how do you feel today? (1: awesome, 10: sad)? " )
+  if feelingToday == "1":
+    print("That's Great! I know, you are the Best!")
+  elif feelingToday == "10":
+    print("Oh, don't worry! I know, you can do it really well!")
+  else:
+    print("I heard, you are doing just fine, keep up the good work until you reach your goal!")
+  
+elif currentDay == "Sunday" or currentDay == "sunday" and currentDay.upper() or currentDay.lower():
+  print("What's your goal for", currentDay.upper())
+  #goalDay7 = input("What's your goal for Day 7? ")
+  feelingToday = input("On a scale of 1 - 10 how do you feel today? (1: awesome, 10: sad) ? " )
+  if feelingToday == "1":
+    print("That's Great! I know, you are the Best!")
+  elif feelingToday == "10":
+    print("Oh, don't worry! I know, you can do it really well!")
+  else:
+    print("I heard, you are doing just fine,keep up the good work until you reach your goal!",name)
+
+
+
+  
+  
+  
+
+
+
+
+  
+
+
+  
+
+
